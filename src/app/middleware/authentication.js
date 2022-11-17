8008268a959f46ff44783cba6ae1f14227590e8d
minhphucdev201 <minhphuc201.dev>
2022-11-17 21:10:07 +0700

    update accessToekn & refreshToken

--- src/app/middleware/authentication.js
+++ src/app/middleware/authentication.js
@@ -9,13 +9,13 @@ const middlewareAuth = {
       const accessToken = token.split(" ")[1];
       jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
-          res.status(403).json("Token is not valid");
+          return res.status(403).json("Token is not valid");
         }
         req.user = user;
         next();
       });
     } else {
-      res.status(401).json({ message: "You're not authenticated" });
+      return res.status(401).json({ message: "You're not authenticated" });
     }
   },
   // verifyToken && verifyAdmin
